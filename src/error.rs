65c539406c4df393aef61b308d9f7dd5654c80db
Austin Hellyer <hello@austinhellyer.me>
2017-01-14 21:47:58 -0800

    Clippy lints

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,6 @@ use std::error::Error as StdError;
 use std::fmt::Display;
 use hyper::Error as HyperError;
 use serde_json::Error as JsonError;
-use serde_json::Value;
 
 /// Common result type used throughout the library.
 pub type Result<T> = ::std::result::Result<T, Error>;
@@ -18,10 +17,6 @@ pub enum Error {
 	Json(JsonError),
 	/// A `std::io` module error
 	Io(IoError),
-	/// A json decoding error, with a description and the offending value
-	Decode(&'static str, Value),
-	/// A miscellaneous error, with a description
-	Other(&'static str),
 }
 
 impl From<IoError> for Error {
@@ -48,9 +43,6 @@ impl Display for Error {
 			Error::Hyper(ref inner) => inner.fmt(f),
 			Error::Json(ref inner) => inner.fmt(f),
 			Error::Io(ref inner) => inner.fmt(f),
-			#[cfg(feature="voice")]
-			Error::Opus(ref inner) => inner.fmt(f),
-			_ => f.write_str(self.description()),
 		}
 	}
 }
@@ -61,8 +53,6 @@ impl StdError for Error {
 			Error::Hyper(ref inner) => inner.description(),
 			Error::Json(ref inner) => inner.description(),
 			Error::Io(ref inner) => inner.description(),
-			Error::Decode(msg, _) => msg,
-			Error::Other(msg) => msg,
 		}
 	}
 
@@ -71,7 +61,6 @@ impl StdError for Error {
 			Error::Hyper(ref inner) => Some(inner),
 			Error::Json(ref inner) => Some(inner),
 			Error::Io(ref inner) => Some(inner),
-			_ => None,
 		}
 	}
 }
