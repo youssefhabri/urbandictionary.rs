927169557aaead4d2313fd3468a0f6b4d9152642
Zeyla Hellyer <zey@zey.moe>
2017-12-31 07:43:23 -0800

    Add non-trait functions for hyper bridge

--- src/bridge/hyper.rs
+++ src/bridge/hyper.rs
@@ -23,45 +23,64 @@ pub trait UrbanDictionaryRequester {
 
 impl UrbanDictionaryRequester for Client<HttpsConnector<HttpConnector>, Body> {
     /// Attempt to retrieve the first `Definition` for a word.
+    #[inline]
     fn define<'a>(&'a self, word: &'a str)
         -> Box<Future<Item = Option<Definition>, Error = Error> + 'a> {
-        let url = format!(
-            "http://api.urbandictionary.com/v0/define?term={}",
-            word,
-        );
-        let uri = match Uri::from_str(&url) {
-            Ok(v) => v,
-            Err(why) => return Box::new(future::err(Error::Uri(why))),
-        };
-
-        Box::new(future::ok(uri)
-            .and_then(move |uri| self.get(uri))
-            .and_then(|res| res.body().concat2())
-            .map_err(From::from)
-            .and_then(|body| serde_json::from_slice::<Response>(&body).map_err(From::from))
-            .map(|mut resp| if !resp.definitions.is_empty() {
-                Some(resp.definitions.remove(0))
-            } else {
-                None
-            }))
+        define(self, word)
     }
 
     /// Attempt to retrieve the definitions of a word.
+    #[inline]
     fn definitions<'a>(&'a self, word: &'a str)
         -> Box<Future<Item = Response, Error = Error> + 'a> {
-        let url = format!(
-            "http://api.urbandictionary.com/v0/define?term={}",
-            word,
-        );
-        let uri = match Uri::from_str(&url) {
-            Ok(v) => v,
-            Err(why) => return Box::new(future::err(Error::Uri(why))),
-        };
-
-        Box::new(future::ok(uri)
-            .and_then(move |uri| self.get(uri))
-            .and_then(|res| res.body().concat2())
-            .map_err(From::from)
-            .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
+        definitions(self, word)
     }
+}
+
+/// Attempt to retrieve the first `Definition` for a word.
+pub fn define<'a>(
+    client: &'a Client<HttpsConnector<HttpConnector>, Body>,
+    word: &'a str,
+) -> Box<Future<Item = Option<Definition>, Error = Error> + 'a> {
+    let url = format!(
+        "http://api.urbandictionary.com/v0/define?term={}",
+        word,
+    );
+    let uri = match Uri::from_str(&url) {
+        Ok(v) => v,
+        Err(why) => return Box::new(future::err(Error::Uri(why))),
+    };
+
+    Box::new(future::ok(uri)
+        .and_then(move |uri| client.get(uri))
+        .and_then(|res| res.body().concat2())
+        .map_err(From::from)
+        .and_then(|body| serde_json::from_slice::<Response>(&body).map_err(From::from))
+        .map(|mut resp| if !resp.definitions.is_empty() {
+            Some(resp.definitions.remove(0))
+        } else {
+            None
+        }))
+}
+
+/// Attempt to retrieve the definitions of a word.
+pub fn definitions<'a>(
+    client: &'a Client<HttpsConnector<HttpConnector>, Body>,
+    word: &'a str,
+) -> Box<Future<Item = Response, Error = Error> + 'a> {
+
+    let url = format!(
+        "http://api.urbandictionary.com/v0/define?term={}",
+        word,
+    );
+    let uri = match Uri::from_str(&url) {
+        Ok(v) => v,
+        Err(why) => return Box::new(future::err(Error::Uri(why))),
+    };
+
+    Box::new(future::ok(uri)
+        .and_then(move |uri| client.get(uri))
+        .and_then(|res| res.body().concat2())
+        .map_err(From::from)
+        .and_then(|body| serde_json::from_slice(&body).map_err(From::from)))
 }
