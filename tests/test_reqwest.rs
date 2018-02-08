aff55f8c9dee997b3f71f738db4fd9a833f3b059
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-02-08 09:29:10 -0800

    Add reqwest tests

---
+++ tests/test_reqwest.rs
@@ -0,0 +1,23 @@
+#![cfg(feature = "reqwest-support")]
+
+extern crate reqwest;
+extern crate urbandictionary;
+
+use reqwest::Client;
+use urbandictionary::ReqwestUrbanDictionaryRequester;
+
+#[ignore]
+#[test]
+fn test_define() {
+    let resp = Client::new().define("england").unwrap();
+
+    assert!(resp.is_some());
+}
+
+#[ignore]
+#[test]
+fn test_definitions() {
+    let resp = Client::new().definitions("england").unwrap();
+
+    assert!(!resp.definitions.is_empty());
+}
